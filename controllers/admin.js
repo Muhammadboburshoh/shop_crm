4ffaea04d08f8d425ccb354b5345dadd914e32a4
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-08-12 16:40:08 +0500

    all products page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getAddProduct = (req, res, next) => {
   const username = req.cookies.__auth.user.login;
   res.render('admin/edit-product', {
@@ -36,6 +38,33 @@ exports.postAddProduct = async (req, res, next) => {
     });
   } catch (err) {
     console.log(err);
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  }
+};
+
+exports.getProducts = async (req, res, next) => {
+  const page = +req.query.page || 1;
+  const username = req.cookies.__auth.user.login;
+
+  try {
+    const { product_count } = await Product.count();
+    const products = await Product.fetchAll(page, ITEMS_PER_PAGE);
+
+    res.render('admin/all-products', {
+      pageTitle: 'All Products',
+      path: 'admin/products',
+      username: username,
+      products: products,
+      currentPage: page,
+      hasNextPage: ITEMS_PER_PAGE * page < product_count,
+      hasPerviousPage: page > 1,
+      nextPage: page + 1,
+      perviousPage: page - 1,
+      lastPage: Math.ceil(product_count / ITEMS_PER_PAGE)
+    });
+  } catch (err) {
     const error = new Error(err);
     error.httpStatusCode = 500;
     return next(error);
--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const adminController = require('../controllers/admin');
 const is_auth = require('../middleware/is-auht');
 
-
 router.get('/add-product', is_auth, adminController.getAddProduct);
+router.get('/products', is_auth, adminController.getProducts);
 router.post('/add-product', is_auth, adminController.postAddProduct);
 
 module.exports = router;
